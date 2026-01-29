262ef1d2c6224f200f834d4c8caf5a3cd9e1e038
gtapiero <giovanny.tapiero@pi.edu.co>
2026-01-29 10:31:27 -0500

    commit - script_empleados :. / .

---
+++ src/model/Empleado.java
@@ -0,0 +1,58 @@
+package model;
+
+public class Empleado {
+
+    private int id;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String cargo;
+
+    public Empleado(int id, String nombre, String apellido, int edad, String cargo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.cargo = cargo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
